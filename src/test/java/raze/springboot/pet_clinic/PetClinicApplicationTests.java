a6be57606580f5ba0f6296c2c4eacdfa3725d1d3
Reza <reza.armiun@yahoo.com>
2019-10-31 14:16:48 +0330

    Initial Project

---
+++ src/test/java/raze/springboot/pet_clinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package raze.springboot.pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
