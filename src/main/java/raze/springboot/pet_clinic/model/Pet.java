6c56bfce69521e712c39cc0d906a1e0d735872b3
Reza <reza.armiun@yahoo.com>
2019-11-01 00:38:11 +0330

    Created Pojo files for pete clinic. Closes #1.

--- src/main/java/raze/springboot/pet_clinic/model/Pet.java
+++ src/main/java/raze/springboot/pet_clinic/model/Pet.java
@@ -4,4 +4,19 @@ public class Pet  {
     private PetType petType;
     private Owner owner;
 
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
 }
