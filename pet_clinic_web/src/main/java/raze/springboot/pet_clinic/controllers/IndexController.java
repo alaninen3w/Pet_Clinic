d5f2897dfaafc9760d8a3fe51868fd562810c3a1
Reza <reza.armiun@yahoo.com>
2019-11-07 00:27:41 +0330

    adding visit service and visit map service implementation. Closes#24

--- pet_clinic_web/src/main/java/raze/springboot/pet_clinic/controllers/IndexController.java
+++ pet_clinic_web/src/main/java/raze/springboot/pet_clinic/controllers/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+
     @RequestMapping({"","/","/index","/index.html"})
     public String index(){
 
